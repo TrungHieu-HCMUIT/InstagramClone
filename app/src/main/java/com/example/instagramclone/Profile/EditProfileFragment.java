2b14cf50054f7e133aa3ed766d3b1ffe88b109b0
TrungHieu-HCMUIT <19521515@gm.uit.edu.vn>
2021-03-03 17:42:18 +0700

    Migrate UniversalImageLoader and set up EditProfile fragment

--- app/src/main/java/com/example/instagramclone/Profile/EditProfileFragment.java
+++ app/src/main/java/com/example/instagramclone/Profile/EditProfileFragment.java
@@ -4,16 +4,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.instagramclone.R;
+import com.example.instagramclone.Utils.UniversalImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class EditProfileFragment extends Fragment {
     private static final String TAG = EditProfileFragment.class.getSimpleName();
 
+    private ImageView mProfilePhoto;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return (ViewGroup) inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        mProfilePhoto = (ImageView) view.findViewById(R.id.profile_photo);
+
+        initImageLoader();
+
+        setProfileImage();
+
+        return view;
+    }
+
+    private void initImageLoader(){
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(getActivity());
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
+    }
+
+    private void setProfileImage(){
+        String imgURL = "instagram.fdad3-2.fna.fbcdn.net/v/t51.2885-19/s150x150/151371215_702610807102333_477374507086313980_n.jpg?tp=1&_nc_ht=instagram.fdad3-2.fna.fbcdn.net&_nc_ohc=YPQpMPZn3jwAX96_zh0&oh=53813b6307bf9722f8231d5af518d4da&oe=606A2AB8";
+        UniversalImageLoader.setImage(imgURL, mProfilePhoto, null, "https://");
     }
 }
